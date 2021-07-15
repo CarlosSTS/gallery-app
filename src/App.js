abf4548c84eec77e694a8d10ca7da795f3e8249e
CarlosSTS <carlossts826@gmail.com>
2021-07-15 09:40:48 -0300

    Configurar statusBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Background from './components/Background'
+import { colors } from './common/colors';
+
 import Routes from './routes'
 import './config/reactotron';
 
 const App = () => {
-console.tron.log('hello')
   return (
     <Background>
       <Routes />
+      <StatusBar
+      barStyle='light-content'
+      backgroundColor={colors.transparent}
+      translucent
+      />
     </Background>
   )
 }
