fd866bb2512f506c89d815496dd6cada7b52a2d5
CarlosSTS <carlossts826@gmail.com>
2021-07-15 16:57:21 -0300

    Adicionar cores

--- src/common/colors.js
+++ src/common/colors.js
@@ -2,5 +2,8 @@ export const colors = {
     initial: '#074885',
     final: '#ab59c1',
     transparent: 'transparent',
-    title: '#fff'
+    white: '#fff',
+    error: '#a00',
+    
+    success: '#0f0'
 }
