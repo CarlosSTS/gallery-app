6a6ab8367b05dd725b16eef7da8175fe756f434d
CarlosSTS <carlossts826@gmail.com>
2021-07-26 14:52:53 -0300

    Configurar redux-resist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar,LogBox } from 'react-native';
+import { StatusBar, LogBox } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
 import Routes from './routes';
@@ -9,21 +10,26 @@ import './config/reactotron';
 import Background from './components/Background'
 import { colors } from './common/colors';
 
-import store from './store';
+import {store,persistor} from './store';
 
-LogBox.ignoreLogs(['Expected style "height:'])
+LogBox.ignoreLogs([
+  'Expected style "height:',
+  'Clipboard has been extracted from react-native core and will be removed in a future release'
+])
 
 const App = () => {
   return (
     <Provider store={store}>
-    <Background>
-      <Routes />
-      <StatusBar
-      barStyle='light-content'
-      backgroundColor={colors.initial}
-      translucent={false}
-      />
-    </Background>
+      <PersistGate persistor={persistor}>
+      <Background>
+        <Routes />
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor={colors.initial}
+          translucent={false}
+        />
+      </Background>
+      </PersistGate>
     </Provider>
   )
 }
